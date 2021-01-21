c6707ca9960fb3693a95c5dffa80c645c7ad5577
Esteban Indiveri <esteban.indiveri@gmail.com>
2021-01-21 17:23:39 -0300

    add bgtheme

--- App.js
+++ App.js
@@ -1,20 +1,73 @@
-import React, { Fragment } from 'react';
-import {SafeAreaView,StyleSheet,ScrollView,View,Text,StatusBar,TouchableWithoutFeedback, Keyboard} from 'react-native';
+import React, { Fragment,useState,useEffect } from 'react';
+import {SafeAreaView,StyleSheet,ScrollView,View,Text,StatusBar,TouchableWithoutFeedback, Keyboard,Alert} from 'react-native';
+import Clima from './components/Clima';
 import Formulario from './components/Formulario';
 
 const App = () => {
+  const [busqueda, setBusqueda] = useState({
+    ciudad:'',
+    pais:'',
+  });
+  const [consultar, setConsultar] = useState(false);
+  const [resultado,setResultado]=useState([]);
+  const [bgColor, setBgColor] = useState('rgb(71,149,212)');
+  const{ciudad,pais}=busqueda;
+  useEffect(() => {
+    const consultarClima=async()=>{
+      if(consultar){
+        const appId='c3ae7264cb6a2c5d1df1cc1ea6c24571';
+        const url=`http://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
+        try{
+          const resp= await fetch(url);
+          const resultado = await resp.json();
+          setResultado(resultado);
+          setConsultar(false);
+          const kelvin=273.15;
+          const {main} = resultado;
+          const actual = main.temp - kelvin;
+          if (parseInt(actual)<10){
+            setBgColor('rgb(105,108,149)');
+          } else if (parseInt(actual) >= 10 && parseInt(actual) < 25){
+            setBgColor('rgb(71,149,212)');
+          } else{
+            setBgColor('rgb(178,28,61)');
+          }
+        } catch (error){
+          mostarAlerta();
+        }
+      }
+    };
+    consultarClima();
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [consultar]);
+
+  const bgColorApp={
+    backgroundColor:bgColor,
+  }
+
+  const mostarAlerta=()=>{
+    Alert.alert(
+        'Error',
+        'No hay resultados, intenta con otra ciudad o país',
+        [{text:'Entendido'}]
+    );
+};
+
   const ocultarTeclado=()=>{
     Keyboard.dismiss();
   };
+
   return (
     <Fragment>
       <StatusBar translucent backgroundColor="transparent"/>
       <TouchableWithoutFeedback
       onPress={()=>ocultarTeclado()}
       >
-      <View style={styles.app}>
+      <View style={[styles.app,bgColorApp]}>
         <View style={styles.contenido}>
-        <Formulario/>
+          <Clima resultado={resultado}/>
+        <Formulario busqueda={busqueda} setBusqueda={setBusqueda} setConsultar={setConsultar}/>
         </View>
       </View>
       </TouchableWithoutFeedback>
@@ -25,7 +78,7 @@ const App = () => {
 const styles = StyleSheet.create({
   app:{
     flex: 1,
-    backgroundColor: 'rgb(71,149,212)',
+    // backgroundColor: 'rgb(71,149,212)',
     justifyContent: 'center',
   },
   contenido:{
--- components/Formulario.js
+++ components/Formulario.js
@@ -1,8 +1,43 @@
-import React, { Fragment } from 'react';
-import { Text, TextInput, View,StyleSheet,TouchableWithoutFeedback } from 'react-native';
+import React, {useState,Fragment,useEffect } from 'react';
+import { Text, TextInput, View,StyleSheet,TouchableWithoutFeedback,Animated, Alert } from 'react-native';
 import {Picker}from '@react-native-community/picker';
 
-const Formulario = () => {
+const Formulario = ({busqueda,setBusqueda,setConsultar}) => {
+    const{pais,ciudad}=busqueda;
+    const [animationbtn] = useState(new Animated.Value(1));
+    const animationStart=()=>{
+        Animated.spring(animationbtn,{
+            toValue: .75,
+            useNativeDriver: true,
+        }).start();
+    };
+    const animatedOut=()=>{
+        Animated.spring(animationbtn,{
+            toValue: 1,
+            useNativeDriver: true,
+            friction:4,
+            tension:30,
+        }).start();
+    };
+    const estiloAnimation={
+        transform:[{
+            scale:animationbtn,
+        }],
+    };
+    const consultarClima=()=>{
+        if(pais.trim()===''||ciudad.trim()===''){
+            mostarAlerta();
+            return;
+        }
+        setConsultar(true);
+    }
+    const mostarAlerta=()=>{
+        Alert.alert(
+            'Error',
+            'Agrega una ciudad y un país a la busqueda',
+            [{text:'Entendido'}]
+        )
+    }
     return (
         <Fragment>
             <View style={styles.formulario}>
@@ -11,11 +46,15 @@ const Formulario = () => {
                     style={styles.input}
                     placeholder="Ciudad"
                     placeholderTextColor= '#668'
+                    value={ciudad}
+                    onChangeText={ciudad=>setBusqueda({...busqueda,ciudad})}
                     />
                 </View>
                 <View>
                     <Picker
-                    itemStyle={{height:120,backgroundColor:'#FFF'}}
+                    onValueChange={(pais)=>setBusqueda({...busqueda,pais})}
+                    selectedValue={pais}
+                    itemStyle={{ height: 120, backgroundColor: 'red' }}
                     >
                         <Picker.Item label="--Seleccione un país --" value=""/>
                         <Picker.Item label="Estados Unidos" value="US"/>
@@ -26,12 +65,16 @@ const Formulario = () => {
                         <Picker.Item label="Perú" value="PE"/>
                     </Picker>
                 </View>
-                <TouchableWithoutFeedback>
-                    <View
-                    style={styles.btnBuscar}
+                <TouchableWithoutFeedback
+                    onPressIn={()=>animationStart()}
+                    onPressOut={()=>animatedOut()}
+                    onPress={()=>consultarClima()}
+                >
+                    <Animated.View
+                    style={[styles.btnBuscar,estiloAnimation]}
                     >
                         <Text style={styles.textoBuscar}>Buscar Clima</Text>
-                    </View>
+                    </Animated.View>
                 </TouchableWithoutFeedback>
             </View>
         </Fragment>
@@ -57,7 +100,7 @@ const Formulario = () => {
          fontWeight: 'bold',
          textTransform: 'uppercase',
          textAlign: 'center',
-         fontSize: 18
+         fontSize: 18,
      },
  })
 export default Formulario;
